248ed8b3796eb06e08d75f67288faae2eeae14d8
Insomn1ac <emelyanoff94@gmail.com>
2021-10-21 13:54:32 +0300

    Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,39 +16,70 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    private static final Properties PROPERTIES = new Properties();
 
     public static void main(String[] args) {
-        try {
+        try (Connection connection = getConnection()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connect", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(readProp())
+                    .withIntervalInSeconds(5)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(connection);
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
-    public static class Rabbit implements Job {
-        @Override
-        public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+    private static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("jdbc.driver"));
+            return DriverManager.getConnection(
+                    config.getProperty("jdbc.url"),
+                    config.getProperty("jdbc.username"),
+                    config.getProperty("jdbc.password")
+            );
         }
     }
 
-    public static int readProp() {
-        try (InputStream is = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            PROPERTIES.load(is);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
         }
-        return Integer.parseInt(PROPERTIES.getProperty("rabbit.interval"));
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+            Connection connect = (Connection) context.getJobDetail().getJobDataMap().get("connect");
+            try (PreparedStatement stmt = connect.prepareStatement("insert into rabbit (created_date) values (current_timestamp)")) {
+                stmt.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static int readProp() {
+            Properties properties = new Properties();
+            try (InputStream is = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+                properties.load(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Integer.parseInt(properties.getProperty("rabbit.interval"));
+        }
     }
 }
