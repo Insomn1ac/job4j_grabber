d4a07f1f92416e8794dd58acf05e5b078a592df5
Insomn1ac <emelyanoff94@gmail.com>
2021-10-25 18:43:16 +0300

    Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,8 +4,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
+    private static final String URL = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
+
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= 5; i++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -19,5 +25,21 @@ public class SqlRuParse {
                 System.out.println("---------------");
             }
         }
+        SqlRuParse grabber = new SqlRuParse();
+        System.out.println(grabber.getTime(URL) + System.lineSeparator() + grabber.getDesc(URL));
+    }
+
+    public String getDesc(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element row = doc.select(".msgBody").get(1);
+        return row.text();
+    }
+
+    public LocalDateTime getTime(String url) throws IOException {
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        Document doc = Jsoup.connect(url).get();
+        Element row = doc.select(".msgFooter").get(0);
+        String str = row.text().split(" \\[")[0];
+        return parser.parse(str);
     }
 }
